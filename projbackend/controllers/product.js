e7634606fb446ae234f0ba683379a0482a465de0
krunal-ctrl <krunaljethva90@gmail.com>
2021-06-10 09:50:48 +0530

    saving photo in monog and tshirt assest

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -1,4 +1,7 @@
 const Product = require("../models/product");
+const formidable = require("formidable");
+const _ = require("lodash");
+const fs = require("fs");
 
 exports.getProductById = (req, res, next, id) => {
   Product.findById(id)
@@ -10,4 +13,47 @@ exports.getProductById = (req, res, next, id) => {
       req.product = product;
       next();
     });
+};
+
+exports.createProduct = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, file) => {
+    if (err) {
+      return res.status(400).json({
+        error: "prblem with image",
+      });
+    }
+    // destructure the fields
+    const { name, description, price, category, stock } = fields;
+    if (!name || !description || !price || !category || !stock) {
+      return res.status(400).json({
+        err: "please include all fields",
+      });
+    }
+
+    let product = new Product(fields);
+
+    //handle file here
+    if (file.photo) {
+      if (file.photo.size > 300000) {
+        return res.status(400).json({
+          error: "file too large",
+        });
+      }
+      product.photo.data = fs.readFileSync(file.photo.path);
+      product.photo.contentType = file.photo.type;
+    }
+
+    // save to database
+    product.save((err, product) => {
+      if (err) {
+        return res.status(err).json({
+          error: "failed to save product",
+        });
+      }
+      res.json(product);
+    });
+  });
 };
--- projbackend/routes/product.js
+++ projbackend/routes/product.js
@@ -4,12 +4,19 @@ const router = express.Router();
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
 
-const { getProductById } = require("../controllers/product");
+const { getProductById, createProduct } = require("../controllers/product");
 
 // all params
 router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 // actual routes
+router.post(
+  "/product/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createProduct
+);
 
 module.exports = router;
